0ffb4e16fb77b8537f4e1bcc65cb8b4118543ba7
polojunarendra-svg <polojunarendra@gmail.com>
2026-07-31 21:45:44 +0530

    L3

--- L3/C/LongestSubstringwithAtMostK .cpp
+++ L3/C/LongestSubstringwithAtMostK .cpp
@@ -7,6 +7,7 @@ int main(void) {
     char string[10]="eceba";
     int k=2;
     char unique[10]="";
+
     for (int i =0;i<strlen(string);i++) {
         for(int j=i;j<strlen(string);j++) {
             if (unique[i]==string[j]) {
