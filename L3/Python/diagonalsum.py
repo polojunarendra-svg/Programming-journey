bdcffe100333913247e447e3828916587da26b71
polojunarendra-svg <polojunarendra@gmail.com>
2026-09-17 20:09:37 +0530

    Hanker rank question

---
+++ L3/Python/diagonalsum.py
@@ -0,0 +1,16 @@
+def diagonalDifference(arr):
+    # Write your code here
+    n = len(arr)
+    sum=0
+    rsum=0
+    for i in range(n):
+        sum+=arr[i][i]
+    for j in range(n):
+            rsum+=arr[j][n-1-j]
+    if sum>rsum:
+        return sum-rsum
+    else:
+        return rsum-sum
+arr =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+result = diagonalDifference(arr)
+print(result)
